ab00aea7ca7529ecd698ac6654a6802117ea283c
Virat Kalshan <virat@Virats-MacBook-Air-8.local>
2026-01-08 02:59:32 +0530

    added websockets without rooms

---
+++ backend/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	conn *websocket.Conn
+	send chan []byte
+}
+
+var (
+	clients   = make(map[*Client]bool)
+	broadcast = make(chan []byte)
+	upgrader  = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+)
+
+func main() {
+	http.HandleFunc("/ws", handleConnections)
+	go handleBroadcast()
+
+	log.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+
+	client := &Client{
+		conn: conn,
+		send: make(chan []byte),
+	}
+
+	clients[client] = true
+
+	go client.write()
+	client.read()
+}
+
+func (c *Client) read() {
+	defer func() {
+		delete(clients, c)
+		c.conn.Close()
+	}()
+
+	for {
+		_, msg, err := c.conn.ReadMessage()
+		if err != nil {
+			return
+		}
+		broadcast <- msg
+	}
+}
+func (c *Client) write() {
+	
+	defer c.conn.Close()
+
+	for msg := range c.send {
+		err := c.conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
+}
+func handleBroadcast() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			select {
+			case client.send <- msg:
+			default:
+				delete(clients, client)
+				close(client.send)
+			}
+		}
+	}
+}
